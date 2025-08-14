8580da6c58ef2834cf4cc947eab5031dd73bc39b
Felipe Malacarne <felipemalacarne012@gmail.com>
2025-08-14 01:33:37 -0300

    Separate Portainer stack

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -15,35 +15,35 @@ terraform {
   }
 }
 
-resource "cloudflare_zero_trust_tunnel_cloudflared" "zamorak_tunnel" {
+resource "cloudflare_zero_trust_tunnel_cloudflared" "portainer_tunnel" {
   account_id    = var.account_id
-  name          = "Terraform Zamorak tunnel"
+  name          = "Terraform Zamorak Portainer tunnel"
   config_src    = "cloudflare"
 }
 
-data "cloudflare_zero_trust_tunnel_cloudflared_token" "zamorak_tunnel_token" {
+data "cloudflare_zero_trust_tunnel_cloudflared_token" "portainer_tunnel_token" {
   account_id   = var.account_id
-  tunnel_id   = cloudflare_zero_trust_tunnel_cloudflared.zamorak_tunnel.id
+  tunnel_id   = cloudflare_zero_trust_tunnel_cloudflared.portainer_tunnel.id
 }
 
-resource "cloudflare_dns_record" "traefik_dns_record" {
+resource "cloudflare_dns_record" "portainer_dns_record" {
   zone_id = var.zone_id
-  name    = "*.${var.zone}"
-  content = "${cloudflare_zero_trust_tunnel_cloudflared.zamorak_tunnel.id}.cfargotunnel.com"
+  name    = "portainer.${var.zone}"
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.portainer_tunnel.id}.cfargotunnel.com"
   type    = "CNAME"
   ttl     = 1
   proxied = true
-  comment = "[terraform] Traefik Ingress Controller DNS record"
+  comment = "[terraform] Portainer Ingress Controller DNS record"
 }
 
-resource "cloudflare_zero_trust_tunnel_cloudflared_config" "zamorak_tunnel_config" {
-  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.zamorak_tunnel.id
+resource "cloudflare_zero_trust_tunnel_cloudflared_config" "portainer_tunnel_config" {
+  tunnel_id  = cloudflare_zero_trust_tunnel_cloudflared.portainer_tunnel.id
   account_id = var.account_id
   config     = {
     ingress   = [
       {
         hostname = "*.${var.zone}"
-        service  = "http://traefik:80"
+        service  = "http://portainer:9000"
       },
       {
         service  = "http_status:404"
--- terraform/cloudflare/outputs.tf
+++ terraform/cloudflare/outputs.tf
@@ -1,20 +1,20 @@
 output "tunnel_id" {
   description = "The ID of the Cloudflare Tunnel"
-  value = cloudflare_zero_trust_tunnel_cloudflared.zamorak_tunnel.id
+  value = cloudflare_zero_trust_tunnel_cloudflared.portainer_tunnel.id
 }
 
 output "tunnel_token" {
   description = "The token for the Cloudflare Tunnel"
-  value      = data.cloudflare_zero_trust_tunnel_cloudflared_token.zamorak_tunnel_token.token
+  value      = data.cloudflare_zero_trust_tunnel_cloudflared_token.portainer_tunnel_token.token
   sensitive   = true
 }
 
 output "tunnel_cname" {
   description = "The CNAME for the Cloudflare Tunnel"
-    value       = cloudflare_dns_record.traefik_dns_record.name
+    value       = cloudflare_dns_record.portainer_dns_record.name
 }
 
 output "tunnel_domain" {
   description = "The full domain for the tunnel"
-  value = "${cloudflare_dns_record.traefik_dns_record.name}.${var.zone}"
+  value = "${cloudflare_dns_record.portainer_dns_record.name}.${var.zone}"
 }
